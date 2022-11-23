76713336c9407c6492143aacb254d38a0b5be7c5
Maiia <maiia.halan@digitallyinspired.com>
2022-11-23 21:00:08 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//alert('Hello');
+//const result = confirm("Are u here?");
+//console.log(result);
+
+//const answer = prompt ("How old are you?", "18");
+//console.log(typeof(answer));
+
+const answer = [];
+//answer [0] = prompt("What is your nam?", "");
+//answer [1] = prompt("How old are you?","");
+//answer [2] = prompt("Where you from?","");
+console.log(typeof(answer));
